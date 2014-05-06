b507d9ceaa457be6e2db1062901a4e965592bfd8
cpiccion <carmelo.piccione@theice.com>
2014-05-06 11:42:36 -0400

    fix install maven plugin script

--- install_maven_plugins.py
+++ install_maven_plugins.py
@@ -5,7 +5,7 @@ import sys
 VERSION = '2.0-early-access11'
 
 # TODO only partially implemented comamnd line arg, do to absolute path not being truncated in the for loop
-MYPATH = getcwd() if len(sys.argv) == 0 else sys.argv[0]
+MYPATH = getcwd() if len(sys.argv) <= 1 else sys.argv[1]
 
 onlyfiles = [ f for f in listdir(MYPATH) if isfile(join(MYPATH,f)) and (str(f)).split('.')[1] == "jar"]
 
