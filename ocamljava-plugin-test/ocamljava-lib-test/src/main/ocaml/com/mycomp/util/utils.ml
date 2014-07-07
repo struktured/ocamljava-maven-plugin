b672c4fa3931b16c45acd43855fc46377e57a9a5
carm <carmelo.piccione@gmail.com>
2014-07-06 22:05:23 -0400

    various cleanups

--- ocamljava-plugin-test/ocamljava-lib-test/src/main/ocaml/com/mycomp/util/utils.ml
+++ ocamljava-plugin-test/ocamljava-lib-test/src/main/ocaml/com/mycomp/util/utils.ml
@@ -4,7 +4,6 @@ open Package'com'mycomp'data'Color
 
 
 let is_public c = c.is_public
-(* This is failing to compile because I need to link javalib somehow during compilation phase *)
 let create_color ~r ~g ~b = 
 	let some_string = Java.make "java.lang.String()" () in 
 	let color = Java.make "com.mycomp.data.Color()" () in
