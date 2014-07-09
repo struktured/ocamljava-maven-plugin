66037f20485bbeb19fda38d1368fa8e293458013
cpiccion <carmelo.piccione@theice.com>
2014-07-09 14:39:53 -0400

    remove a build warning

--- ocamljava-plugin-test/ocamljava-lib-test/src/main/ocaml/com/mycomp/util/utils.ml
+++ ocamljava-plugin-test/ocamljava-lib-test/src/main/ocaml/com/mycomp/util/utils.ml
@@ -5,7 +5,7 @@ open Package'com'mycomp'data'Color
 
 let is_public c = c.is_public
 let create_color ~r ~g ~b = 
-	let some_string = Java.make "java.lang.String()" () in 
+	let _ = Java.make "java.lang.String()" () in
 	let color = Java.make "com.mycomp.data.Color()" () in
 	let to_java_string = Java.call "Color.toString()" color in
 	let to_string = JavaString.to_string to_java_string in
